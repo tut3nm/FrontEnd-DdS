adc659f7d63862fad49660283afcd3b0f5e3fd8e
Mateo Cortez Quintino <mateocortezquintino@gmail.com>
2025-06-18 18:46:27 -0300

    asd

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { AuthProvider } from './auth/AuthProvider';
+import { AuthProvider } from './Components/AuthProvider';
 import ProtectedRoute from './components/ProtectedRoute';
 
 import Navbar from './components/Navbar';
