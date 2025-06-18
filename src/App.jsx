0dbea141705a69edf0b5460217c4989e8d7a72fd
Mateo Cortez Quintino <mateocortezquintino@gmail.com>
2025-06-18 18:45:58 -0300

    app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,87 @@
-import { useState } from 'react'
-import './App.css'
-import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
-import Home from './Components/Home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './auth/AuthProvider';
+import ProtectedRoute from './components/ProtectedRoute';
 
-function AppContent() {
-  const [count, setCount] = useState(0)
+import Navbar from './components/Navbar';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Home from './pages/Home';
+import Phones from './pages/Phones';
+import PhoneDetail from './pages/PhoneDetail';
+import Watches from './pages/Watches';
+import WatchDetail from './pages/WatchDetail';
+import Brands from './pages/Brands';
+import Compare from './pages/Compare';
 
+function App() {
   return (
-    <>
-      <main className="main-content">
+    <AuthProvider>
+      <Router>
+        <Navbar />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+
+          <Route
+            path="/"
+            element={
+              <ProtectedRoute>
+                <Home />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/phones"
+            element={
+              <ProtectedRoute>
+                <Phones />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/phones/:id"
+            element={
+              <ProtectedRoute>
+                <PhoneDetail />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/watches"
+            element={
+              <ProtectedRoute>
+                <Watches />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/watches/:id"
+            element={
+              <ProtectedRoute>
+                <WatchDetail />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/brands"
+            element={
+              <ProtectedRoute>
+                <Brands />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/compare"
+            element={
+              <ProtectedRoute>
+                <Compare />
+              </ProtectedRoute>
+            }
+          />
         </Routes>
-      </main>
-    </>
-  )
+      </Router>
+    </AuthProvider>
+  );
 }
 
-function App(){
-  return(
-    <Router>
-      <AppContent/>
-    </Router>
-  )
-}
-export default App
+export default App;
